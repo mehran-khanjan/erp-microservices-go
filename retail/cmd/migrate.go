c02b225f7a329410f262a4f387936eb76b53a684
unknown <mehrankh1913@gmail.com>
2024-01-22 20:54:30 +0330

    add more codes

--- retail/cmd/migrate.go
+++ retail/cmd/migrate.go
@@ -3,3 +3,12 @@ package cmd
 func init() {
 	rootCmd.AddCommand(migrateCmd)
 }
+
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "Table migration",
+	Long:  "Table migration",
+	Run: func(cmd *cobra.Command, args []string) {
+		migrate()
+	},
+}
