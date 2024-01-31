b26e622023b1b5d2c6ecc15b953a365d1e8f131d
unknown <mehrankh1913@gmail.com>
2024-01-31 13:47:11 +0330

    add more codes

--- retail/cmd/root.go
+++ retail/cmd/root.go
@@ -1,7 +1,19 @@
 package cmd
 
+import (
+	"fmt"
+	"os"
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "help",
 	Short: "Help command",
 	Long:  "Display help command",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
