7861452463e18ddd2c9723c0c64b3667056c1d04
unknown <mehrankh1913@gmail.com>
2024-02-05 09:05:18 +0330

    add more codes

--- retail/cmd/seed.go
+++ retail/cmd/seed.go
@@ -3,3 +3,12 @@ package cmd
 func init() {
 	rootCmd.AddCommand(seedCmd)
 }
+
+var seedCmd = &cobra.Command{
+	Use:   "seed",
+	Short: "Database seeder",
+	Long:  "Database seeder",
+	Run: func(cmd *cobra.Command, args []string) {
+		seed()
+	},
+}
