57c016f6d5bf1069a0e57632d1174245e83765f1
unknown <mehrankh1913@gmail.com>
2024-02-16 18:30:06 +0330

    add more codes

--- retail/cmd/serve.go
+++ retail/cmd/serve.go
@@ -3,3 +3,12 @@ package cmd
 func init() {
 	rootCmd.AddCommand(serveCmd)
 }
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Serve app on dev server",
+	Long:  "Application will be served on host and port defined in config.yml file",
+	Run: func(cmd *cobra.Command, args []string) {
+		serve()
+	},
+}
