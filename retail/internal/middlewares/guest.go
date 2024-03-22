6c7e78cdab55c70965fca4a0028e2cad1bccd2ce
unknown <mehrankh1913@gmail.com>
2024-03-22 09:54:03 +0330

    add more codes

--- retail/internal/middlewares/guest.go
+++ retail/internal/middlewares/guest.go
@@ -1,5 +1,25 @@
 package middlewares
 
+import (
+	"net/http"
+	"strconv"
+)
+
 func IsGuest() gin.HandlerFunc {
 	var userRepo = UserRepository.New()
+
+	return func(c *gin.Context) {
+		authID := sessions.Get(c, "auth")
+		userID, _ := strconv.Atoi(authID)
+
+		user := userRepo.FindByID(userID)
+
+		if user.ID != 0 {
+			c.Redirect(http.StatusFound, "/")
+			return
+		}
+		// before request
+
+		c.Next()
+	}
 }
