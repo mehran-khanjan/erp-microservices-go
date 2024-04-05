3bdf0abb6d0dccd6f4fc9b9196dc1476475e300f
unknown <mehrankh1913@gmail.com>
2024-04-05 10:16:44 +0330

    add more codes

--- retail/modules/article/controllers/article_controller.go
+++ retail/modules/article/controllers/article_controller.go
@@ -1,5 +1,10 @@
 package controllers
 
+import (
+	"net/http"
+	"strconv"
+)
+
 type Controller struct {
 	articleService ArticleService.ArticleServiceInterface
 }
@@ -7,5 +12,14 @@ type Controller struct {
 func New() *Controller {
 	return &Controller{
 		articleService: ArticleService.New(),
+	}
+}
+
+func (controller *Controller) Show(c *gin.Context) {
+	// Get the article
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		html.Render(c, http.StatusInternalServerError, "templates/errors/html/500", gin.H{"title": "Server error", "message": "error converting the id"})
+		return
 	}
 }
