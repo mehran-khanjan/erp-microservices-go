3b71651ba7ae6520df41f1b433aa3014c8ccadf6
Mehran Khanjan <mehrankh1913@gmail.com>
2024-06-12 09:38:17 +0400

    add more codes

--- retail/modules/article/repositories/article_repository.go
+++ retail/modules/article/repositories/article_repository.go
@@ -31,3 +31,11 @@ func (articleRepository *ArticleRepository) Find(id int) ArticleModel.Article {
 
 	return article
 }
+
+func (articleRepository *ArticleRepository) Create(article ArticleModel.Article) ArticleModel.Article {
+	var newArticle ArticleModel.Article
+
+	articleRepository.DB.Create(&article).Scan(&newArticle)
+
+	return newArticle
+}
