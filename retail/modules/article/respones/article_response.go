f5a5fa1fefd96bb86dbce61546e78362c3a3451b
Mehran Khanjan <mehrankh1913@gmail.com>
2024-06-26 10:39:19 +0400

    add more codes

--- retail/modules/article/respones/article_response.go
+++ retail/modules/article/respones/article_response.go
@@ -18,3 +18,14 @@ type Article struct {
 type Articles struct {
 	Data []Article
 }
+
+func ToArticle(article ArticleModel.Article) Article {
+	return Article{
+		ID:        article.ID,
+		Title:     article.Title,
+		Content:   article.Content,
+		Image:     "/assets/img/demopic/10.jpg",
+		CreatedAt: fmt.Sprintf("%d/%02d/%02d", article.CreatedAt.Year(), article.CreatedAt.Month(), article.CreatedAt.Day()),
+		User:      UserResponse.ToUser(article.User),
+	}
+}
